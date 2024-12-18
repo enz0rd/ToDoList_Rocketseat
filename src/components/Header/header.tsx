420e6a574c846e949542e597bfd1566f440485fb
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2024-12-18 16:29:11 -0300

    added attribution

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,11 +4,14 @@ import styles from './header.module.css'
 export function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.rocket} src={rocket} width={22} height={36} />
-      <h1 className={styles.title}>
-        <strong className={styles.to}>to</strong>
-        <strong className={styles.do}>do</strong>
-      </h1>
+      <div className={styles.header_title}>
+        <img className={styles.rocket} src={rocket} width={22} height={36} />
+        <h1 className={styles.title}>
+          <strong className={styles.to}>to</strong>
+          <strong className={styles.do}>do</strong>
+        </h1>
+      </div>
+      <span>por <a href='https://github.com/enz0rd' target='_blank'>enz0rd</a></span>
     </header>
   );
 }
